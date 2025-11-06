c71abe41756878db77447ac7027cdd82d9787d31
xaxam2001 <maximecha14@gmail.com>
2025-11-06 18:06:55 +0100

    pass regression test with MLP python implementation

---
+++ ML_lib/MLP.cpp
@@ -0,0 +1,5 @@
+//
+// Created by maxim on 06/11/2025.
+//
+
+#include "MLP.h"
---
+++ ML_lib/MLP.hpp
@@ -0,0 +1,13 @@
+//
+// Created by maxim on 06/11/2025.
+//
+
+#ifndef ML_LIB_MLP_H
+#define ML_LIB_MLP_H
+
+
+class MLP {
+};
+
+
+#endif //ML_LIB_MLP_H
